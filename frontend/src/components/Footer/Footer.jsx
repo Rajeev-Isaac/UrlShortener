c557b56493e9f8b89089c70de53cd996f6889c53
Rajeev-Isaac <rajeevisaac12@gmail.com>
2025-04-12 01:15:00 +0530

    Initial commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-900 text-white text-base text-center py-5 '>
+      A Project by Rajeev Isaac
+    </div>
+  )
+}
+
+export default Footer
